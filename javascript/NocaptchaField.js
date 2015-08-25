323de9141d1ea6a5ce432f239fd369bf7a77fd08
Richard Rudy <rick@nervedesigns.com>
2015-08-25 15:32:10 -0400

    Added Callbck Function Check

--- javascript/NocaptchaField.js
+++ javascript/NocaptchaField.js
@@ -3,7 +3,12 @@ var _noCaptchaFields=_noCaptchaFields || [];
 function noCaptchaFieldRender() {
     for(var i=0;i<_noCaptchaFields.length;i++) {
         var field=document.getElementById('Nocaptcha-'+_noCaptchaFields[i]);
-        var options={'sitekey': field.getAttribute('data-sitekey'), 'theme': field.getAttribute('data-theme'), 'type': field.getAttribute('data-type')};
+        var options={
+            'sitekey': field.getAttribute('data-sitekey'),
+            'theme': field.getAttribute('data-theme'),
+            'type': field.getAttribute('data-type'),
+            'callback': (field.getAttribute('data-callback') ? verifyCallback : undefined )
+        };
         
         grecaptcha.render(field, options);
     }
