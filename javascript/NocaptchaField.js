112b65520d8ae8a1f72e4152200c753de018f01d
Scott Hutchinson <scott@silverstripe.com>
2021-09-14 15:10:27 +1200

    ENHANCEMENT: Add ability to add exempt actions from recaptcha checks
    If actions have been set as exempt from validation on the Silverstripe Form then we skip it from recaptcha checks

--- javascript/NocaptchaField.js
+++ javascript/NocaptchaField.js
@@ -1,12 +1,20 @@
 var _noCaptchaFields=_noCaptchaFields || [];
+var _noCaptchaValidationExemptActions=_noCaptchaValidationExemptActions || [];
 
 function noCaptchaFieldRender() {
     var submitListener=function(e) {
+        // If the action is exempt from validation, skip any recaptcha checks
+        if (e.submitter &&
+            e.submitter.name &&
+            _noCaptchaValidationExemptActions.indexOf(e.submitter.name.substring(7)) > -1) {
+            return;
+        }
+
         e.preventDefault();
         var widgetID = e.target.querySelectorAll('.g-recaptcha')[0].getAttribute('data-widgetid');
         grecaptcha.execute(widgetID);
     };
-    
+
     var render = function(field) {
         var options={
             'sitekey': field.getAttribute('data-sitekey'),
@@ -15,12 +23,12 @@ function noCaptchaFieldRender() {
             'size': field.getAttribute('data-size'),
             'badge': field.getAttribute('data-badge'),
         };
-        
+
         //For the invisible captcha we need to setup some callback listeners
         if(field.getAttribute('data-size')=='invisible' && field.getAttribute('data-callback')==null) {
             var form=document.getElementById(field.getAttribute('data-form'));
             var superHandler=false;
-            
+
             if(typeof jQuery!='undefined' && typeof jQuery.fn.validate!='undefined') {
                 var formValidator=jQuery(form).data('validator');
                 if (!formValidator) {
@@ -40,7 +48,7 @@ function noCaptchaFieldRender() {
                     console.error('Could not attach event to the form');
                 }
             }
-            
+
             window['Nocaptcha-'+_noCaptchaFields[i]]=function() {
                 return new Promise(function(resolve, reject) {
                     if(typeof jQuery!='undefined' && typeof jQuery.fn.validate!='undefined' && superHandler) {
@@ -48,20 +56,20 @@ function noCaptchaFieldRender() {
                     }else {
                         form.submit();
                     }
-                    
+
                     resolve();
                 });
             };
-            
+
             options.callback = 'Nocaptcha-'+_noCaptchaFields[i];
         } else if (field.getAttribute('data-callback')) {
             options.callback = field.getAttribute('data-callback');
         }
-        
+
         var widget_id = grecaptcha.render(field, options);
         field.setAttribute("data-widgetid", widget_id);
     }
-    
+
     for(var i=0;i<_noCaptchaFields.length;i++) {
         render(document.getElementById('Nocaptcha-'+_noCaptchaFields[i]));
     }
